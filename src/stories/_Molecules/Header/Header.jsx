2af7379a692511b3c635fd61065942bf687b41c3
Sui Gn <natsynapses@gmail.com>
2024-09-13 12:44:47 -0500

    -
    -

--- src/stories/_Molecules/Header/Header.jsx
+++ src/stories/_Molecules/Header/Header.jsx
@@ -24,7 +24,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             />
           </g>
         </svg>
-        <h1>Acme</h1>
+        <h1>THIS.GUI</h1>
       </div>
       <div>
         {user ? (
