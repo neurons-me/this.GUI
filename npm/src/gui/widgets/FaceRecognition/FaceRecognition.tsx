15f5b1b7536bd2bce47f0870dbfe8b02e4ac3bb0
neurons.me <admin@neurons.me>
2026-03-25 14:32:15 -0600

    -

--- npm/src/gui/widgets/FaceRecognition/FaceRecognition.tsx
+++ npm/src/gui/widgets/FaceRecognition/FaceRecognition.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useRef, useState, useCallback } from 'react';
 import Webcam from 'react-webcam';
 import { Box, Button, Typography } from '@/gui/Atoms/atoms';
-import { Modal } from '@/gui/Molecules/molecules';
+import { Modal } from '@/gui/Molecules';
 import { useFaceLandmarker } from './modules/useFaceLandmarker';
 import { useFaceCameraPermission } from './modules/useFaceCameraPermission';
 import { useFaceOverlay } from './modules/useFaceOverlay';
